b3cedd76d233c7b4b151624c4120e085f2c8949c
samuel <samueltsutcliffe@gmail.com>
2025-02-11 12:18:37 +0000

    added some test data for the users, as well as created a view that allows for the real password hash to not be shown

--- Sam/database.sql
+++ Sam/database.sql
@@ -42,6 +42,42 @@ laptop VARCHAR(80),
 id int PRIMARY KEY,
 uname VARCHAR(50) NOT NULL,
 phonenum int NOT NULL,
-email VARCHAR(50) NULL
+email VARCHAR(50) NULL,
+password VARCHAR(200) NOT NULL
 );
 
+ALTER TABLE users
+ALTER COLUMN uname TYPE VARCHAR(20);
+
+ALTER TABLE users
+ALTER COLUMN password TYPE VARCHAR(512);
+
+ALTER TABLE users DROP COLUMN id;
+
+ALTER TABLE users 
+ADD COLUMN id SERIAL PRIMARY KEY; 
+
+INSERT INTO users (uname, phonenum, email, password)
+VALUES 
+-- password BlueSky$2023!
+    ('quantumpanda', '02079460958', 'quantum.panda@example.com', 'e3d6b4f5a6b1c2e8f9a0d7b8c6d5e4f3a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6'),
+	-- password Tiger@Moon99
+    ('StarlightVoyager', '0131490123', 'starlight.voyager@example.com', 'a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2'),
+	-- password Ocean#Wave123
+    ('NeonFalcon', '01614993456', 'neon.falcon@example.com', 'b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3'),
+	-- password Fire&Ice_456
+    ('MysticGrizzly', '01214967890', 'mystic.grizzly@example.com', 'c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4'),
+	-- password Star^Night789
+    ('CyberPhoenix', '01414962345', 'cyber.phoenix@example.com', 'd4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5');
+
+-- this view shows the users information without showing the actual password hash, by making a fake password
+CREATE VIEW user_info AS
+SELECT
+	uname, 
+	phonenum,
+	email,
+	substring(md5(random()::text) FROM 1 FOR 10) AS fake_password
+	FROM users;
+
+SELECT * FROM user_info;
+-- selects the view
