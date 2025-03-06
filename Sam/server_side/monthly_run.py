ab9f8a0ee23bb4bfebb45f14608404468dd0bdfe
samuel <samueltsutcliffe@gmail.com>
2025-03-06 12:59:25 +0000

    deleted the logs and we now have one set of logs, to show everything that is happening when the monthly_run.py runs

--- Sam/server_side/monthly_run.py
+++ Sam/server_side/monthly_run.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 cron = CronTab(user=True)
-job = cron.new(command="python3 /home/samuel/Documents/2_Brighton/sem2/GroupProject/laptop_chatbot/Sam/server_side/run_all.py >> /home/samuel/Documents/2_Brighton/sem2/GroupProject/laptop_chatbot/Sam/server_side/out/output_.log 2>&1")
+job = cron.new(command="python3 /home/samuel/Documents/2_Brighton/sem2/GroupProject/laptop_chatbot/Sam/server_side/run_all.py >> /home/samuel/Documents/2_Brighton/sem2/GroupProject/laptop_chatbot/Sam/server_side/out/output_log.log 2>&1")
 job.minute.every(5) # run every 5 minutes
 # job.day.on(1) # runs every 1st of the month
 
