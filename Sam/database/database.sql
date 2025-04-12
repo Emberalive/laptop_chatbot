b257e08eac9fefb90a54a74844e770049f0f0fb0
Sammy Boy <samueltsutcliffe@gmail.com>
2025-04-12 17:48:23 +0100

    didnt commit the schema changes

--- Sam/database/database.sql
+++ Sam/database/database.sql
@@ -7,7 +7,7 @@ CREATE TABLE laptops (
     memory_installed character varying(20) NOT NULL,
     operating_system character varying(50) DEFAULT 'none'::character varying,
     screen_size character varying(12) NOT NULL,
-    PRIMARY KEY (model, weight, memory_installed)
+    PRIMARY KEY (model, weight, screen_size)
 );
 
 CREATE TABLE cpu (
@@ -15,9 +15,9 @@ CREATE TABLE cpu (
     brand character varying(50),
     laptop_model character varying(80) NOT NULL,
     laptop_weight character varying(10) NOT NULL,
-    laptop_memory character varying(20) NOT NULL,
-    FOREIGN KEY (laptop_model, laptop_weight, laptop_memory) REFERENCES laptops(model, weight, memory_installed),
-    PRIMARY KEY (model, laptop_model, laptop_weight, laptop_memory)
+    laptop_screen character varying(12) NOT NULL,
+    FOREIGN KEY (laptop_model, laptop_weight, laptop_screen) REFERENCES laptops(model, weight, screen_size),
+    PRIMARY KEY (model, laptop_model, laptop_weight, laptop_screen)
 );
 
 CREATE TABLE gpu (
@@ -25,54 +25,54 @@ CREATE TABLE gpu (
     brand character varying(50),
     laptop_model character varying(80) NOT NULL,
     laptop_weight character varying(10) NOT NULL,
-    laptop_memory character varying(20) NOT NULL,
-    FOREIGN KEY (laptop_model, laptop_weight, laptop_memory) REFERENCES laptops(model, weight, memory_installed),
-    PRIMARY KEY (model, laptop_model, laptop_weight, laptop_memory)
+    laptop_screen character varying(12) NOT NULL,
+    FOREIGN KEY (laptop_model, laptop_weight, laptop_screen) REFERENCES laptops(model, weight, screen_size),
+    PRIMARY KEY (model, laptop_model, laptop_weight, laptop_screen)
 );
 
 CREATE TABLE features (
     laptop_model character varying(80) NOT NULL,
     laptop_weight character varying(10) NOT NULL,
-    laptop_memory character varying(20) NOT NULL,
+    laptop_screen character varying(12) NOT NULL,
     bluetooth character varying(10) NOT NULL,
     num_pad character varying(10) NOT NULL,
     backlit_keyboard character varying(10) NOT NULL,
-    FOREIGN KEY (laptop_model, laptop_weight, laptop_memory) REFERENCES laptops(model, weight, memory_installed),
-    PRIMARY KEY (laptop_model, laptop_weight, laptop_memory)
+    FOREIGN KEY (laptop_model, laptop_weight, laptop_screen) REFERENCES laptops(model, weight, screen_size),
+    PRIMARY KEY (laptop_model, laptop_weight, laptop_screen)
 );
 
 CREATE TABLE ports (
     laptop_model character varying(80) NOT NULL,
     laptop_weight character varying(10) NOT NULL,
-    laptop_memory character varying(20) NOT NULL,
+    laptop_screen character varying(12) NOT NULL,
     hdmi character varying(10) NOT NULL,
     ethernet character varying(10) NOT NULL,
     thunderbolt character varying(10) NOT NULL,
     type_c character varying(10) NOT NULL,
     display_port character varying(10) NOT NULL,
-    FOREIGN KEY (laptop_model, laptop_weight, laptop_memory) REFERENCES laptops(model, weight, memory_installed),
-    PRIMARY KEY (laptop_model, laptop_weight, laptop_memory)
+    FOREIGN KEY (laptop_model, laptop_weight, laptop_screen) REFERENCES laptops(model, weight, screen_size),
+    PRIMARY KEY (laptop_model, laptop_weight, laptop_screen)
 );
 
 CREATE TABLE screen (
     laptop_model character varying(80) NOT NULL,
     laptop_weight character varying(10) NOT NULL,
-    laptop_memory character varying(20) NOT NULL,
+    laptop_screen character varying(12) NOT NULL,
     screen_resolution character varying(20) NOT NULL,
     refresh_rate character varying(20) NOT NULL,
     touch_screen character varying(10) NOT NULL,
-    FOREIGN KEY (laptop_model, laptop_weight, laptop_memory) REFERENCES laptops(model, weight, memory_installed),
-    PRIMARY KEY (laptop_model, laptop_weight, laptop_memory)
+    FOREIGN KEY (laptop_model, laptop_weight, laptop_screen) REFERENCES laptops(model, weight, screen_size),
+    PRIMARY KEY (laptop_model, laptop_weight, laptop_screen)
 );
 
 CREATE TABLE storage (
     laptop_model character varying(80) NOT NULL,
     laptop_weight character varying(10) NOT NULL,
-    laptop_memory character varying(20) NOT NULL,
+    laptop_screen character varying(12) NOT NULL,
     storage_amount character varying(15) NOT NULL,
     storage_type character varying(15) NOT NULL,
-    FOREIGN KEY (laptop_model, laptop_weight, laptop_memory) REFERENCES laptops(model, weight, memory_installed),
-    PRIMARY KEY (laptop_model, laptop_weight, laptop_memory)
+    FOREIGN KEY (laptop_model, laptop_weight, laptop_screen) REFERENCES laptops(model, weight, screen_size),
+    PRIMARY KEY (laptop_model, laptop_weight, laptop_screen)
 );
 
 CREATE TABLE users (
