1c085c014c72b7f3ef7f57a19dfc1a3720a298fd
samuel <samueltsutcliffe@gmail.com>
2025-05-05 15:10:47 +0100

    I have added a try catch and called pprint properly for comparison.py

--- Sam/server_side/scrapers/comparison.py
+++ Sam/server_side/scrapers/comparison.py
@@ -1,11 +1,25 @@
 from deepdiff import DeepDiff
 import json
 import pprint
+import sys
+from loguru import logger
 
-with open('/home/samuel/laptop_chatbot/Sam/server_side/scrapers/scraped_data/latest.json') as f1, \
-     open('/home/samuel/laptop_chatbot/Sam/server_side/scrapers/scraped_data/old_data/scrape_2025-05-05.json') as f2:
-    data1 = json.load(f1)
-    data2 = json.load(f2)
+logger.add(sys.stdout, format="{time} {level} {message}")
+logger.add("../logs/comparison.log", rotation="10 MB", retention="35 days", compression="zip")
+logger = logger.bind(user="comparer")
 
-diff = DeepDiff(data1, data2, verbose_level=2)
-pprint(diff)
+logger.info(f"starting the comparison between the od and new json files")
+try:
+    with open('/home/samuel/laptop_chatbot/Sam/server_side/scrapers/scraped_data/latest.json') as f1, \
+         open('/home/samuel/laptop_chatbot/Sam/server_side/scrapers/scraped_data/old_data/scrape_2025-05-05.json') as f2:
+        data1 = json.load(f1)
+        data2 = json.load(f2)
+
+    diff = DeepDiff(data1, data2, verbose_level=2)
+
+    logger.info("comparison completed")
+
+    pprint.pprint(diff)
+except Exception as e:
+    logger.error(f"error in attempting to compare the old and new json data {e}")
+pprint.pprint(diff)
