67004cb236003d1070305ff4c1a31aa2c3b1603b
samuel <samueltsutcliffe@gmail.com>
2025-05-12 12:13:27 +0100

    I have changed the path for the logs so that they are in the same directory for comparison.py I have also changed it so that the database connections are released after all the threads have completed, making the connections safer as well as actually commiting the inserts, and adding a log indicating what config was added as well as adding the directory to the old scraped_json to the .env file. I have also commented out the old get connection function inside of dbAccess.py as we dont need it anymore

--- Sam/server_side/DBAccess/dbAccess.py
+++ Sam/server_side/DBAccess/dbAccess.py
@@ -55,20 +55,20 @@ def release_db_connection(conn, cur):
     except Exception as e:
         logger.error(f"Error releasing connection: {e}")
 
-def db_access():
-    try:
-        print("Attempting connection to the database\n")
-        # put this is your name and password until we're at point where we can insert
-        conn = psycopg2.connect(database = "laptopchatbot",
-            user = "samuel",
-            host = "86.19.219.159",
-            password = "QwErTy1243!",
-            port = 5432)
-
-        # Creating a cursor with name cur, allows queries to the db
-        cur = conn.cursor()
-        print('Connected to the PostgreSQL database\n')
-        return conn, cur
-    except Exception as e:
-        print("connection was not made. Error: {e}")
+# def db_access():
+#     try:
+#         print("Attempting connection to the database\n")
+#         # put this is your name and password until we're at point where we can insert
+#         conn = psycopg2.connect(database = "laptopchatbot",
+#             user = "samuel",
+#             host = "86.19.219.159",
+#             password = "QwErTy1243!",
+#             port = 5432)
+#
+#         # Creating a cursor with name cur, allows queries to the db
+#         cur = conn.cursor()
+#         print('Connected to the PostgreSQL database\n')
+#         return conn, cur
+#     except Exception as e:
+#         print("connection was not made. Error: {e}")
 
