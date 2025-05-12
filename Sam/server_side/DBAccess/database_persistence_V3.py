e1428663e4526c6dcb6a13c4f3ad78eabf47c367
samuel <samueltsutcliffe@gmail.com>
2025-05-13 00:52:28 +0100

    i changed the check a bit as i assigned the return value to a variable, so I made the check cursor.fetchone()[0] use the logic that grabbed the value. as it was returning an error so i did this

--- Sam/server_side/DBAccess/database_persistence_V3.py
+++ Sam/server_side/DBAccess/database_persistence_V3.py
@@ -102,10 +102,9 @@ def insert_configuration(model_id, price, weight, battery_life, memory_installed
         config_id = cursor.fetchone()[0]
         db_connection.commit()
         logger_server.info(f"Inserted laptop_configurations with config_id: {config_id}")
-        config_id = cursor.fetchone()[0]
-        if not config_id:
+        if not cursor.fetchone()[0]:
             return None
-        return config_id
+        return cursor.fetchone()[0]
     except Exception as config_insert_error:
         db_connection.rollback()
         logger_server.error(f"Error inserting laptop_configuration: {config_insert_error}")
